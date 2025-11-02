51f7f9b8c3b539d9d5c4f64baa459b84d0db6217
Fless <132740827+flessan@users.noreply.github.com>
2025-11-03 04:57:47 +0800

    please?

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+export default defineNuxtConfig({
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio',
+      gitInfo: {
+        name: 'docus',
+        owner: 'flessan',
+        url: 'https://github.com/flessan/docus'
+      }
+    }
+  }
+})
